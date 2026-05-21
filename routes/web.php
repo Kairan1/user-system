f885cce5aa1f2a56cf06dc57a25e663c41c277fc
umar <abdulazeezumar2004@gmail.com>
2026-05-22 00:07:03 +0800

    welcome edited

--- routes/web.php
+++ routes/web.php
@@ -7,7 +7,13 @@ Route::get('/', function () {
 });
 use App\Http\Controllers\UserController;
 
-Route::get('/', [UserController::class, 'index']);
+// Route::get('/', [UserController::class, 'index']);
+Route::get('/register', [UserController::class, 'index']);
 Route::post('/register', [UserController::class, 'store']);
+
+Route::get('/search', function () {
+    return view('search');
+});
 Route::post('/search', [UserController::class, 'search']);
+
 Route::get('/delete/{id}', [UserController::class, 'delete']);
